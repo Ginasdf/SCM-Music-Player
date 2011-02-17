bf9fe20ed77d7f37e65874abbede1e2e74da29d0
Chun Ming Shum <shumchunming@gmail.com>
2011-02-17 22:39:45 +0000

    tiny updates

--- container.php
+++ container.php
@@ -46,7 +46,61 @@ $playerTop = $_GET['place']!='bottom';
 $showPlaylist = $_GET['showplaylist']=='true';
 $url=fixURL($_GET['url']);
 
-?>
+if($data==""){ ?>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Lorum Ipsum</title>
+</head>
+<body>
+<div style="width:700px;margin:10px auto;">
+<h1>Lorum Ipsum</h1>
+
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6799082498278077";
+/* SCM container */
+google_ad_slot = "6521832710";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+ 
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6799082498278077";
+/* SCM container */
+google_ad_slot = "6521832710";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+
+</div>
+
+</body>
+</html>
+
+
+
+<?php }else{ ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -55,3 +109,5 @@ $url=fixURL($_GET['url']);
 </head>
 <?php echo generateFrames($url,$data,$playerTop,$showPlaylist); ?>
 </html>
+
+<?php } ?>
